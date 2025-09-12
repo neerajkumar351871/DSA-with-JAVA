c78658636edd3de4d71fd7c842ea3138dd31f9da
neerajkumar351871 <neerajkumar351871@gmail.com>
2025-09-12 22:21:03 +0530

    New Update

--- Day-12/Hollow.java
+++ Day-12/Hollow.java
@@ -1,28 +1,32 @@
-public class Hollow
+public class hollow
 {
-  public static void hollowprint(int row, int colu)
-  {  // Column
-   
-    for(int i=1;i<=colu;i++)
+    public static void HollowRhombus(int n)
     {
-         // Row
-         for(int j=1;j<=row;j++)
-         {
-            if(i==1 || i==colu || j==1 || j==row)
-            {
-                System.out.print("*");
-            }
-            else
+        // Inner loop 
+        for(int i=1;i<=n;i++)
+        {
+            // Space
+            for(int j=1;j<=(n-i);j++)
             {
                 System.out.print(" ");
             }
-           
-         }
-        System.out.println();
+            // Star
+            for( int k=1; k<=n;k++)
+            {
+                if(i==1||i==n||k==1||k==n)
+                {
+                    System.out.print("*");
+                }
+                else
+                {
+                    System.out.print(" ");
+                }
+                }
+                System.out.println( );
+        }
     }
+  public static void main(String args[])
+  {
+     HollowRhombus(7);
   }
-public static void main(String arg[])
-{
-    hollowprint(4,5);
-}
 }
